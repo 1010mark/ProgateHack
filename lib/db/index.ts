ed888484ad8a332de14655082f0c56ca84dd7ed9
1010mark <markmentaro@yahoo.co.jp>
2025-03-22 16:58:06 +0900

    sslll

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -6,7 +6,7 @@ const settings = {
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  ssl: false,
+  ssl: true,
   extra: {
     ssl: { rejectUnauthorized: false } 
   }
