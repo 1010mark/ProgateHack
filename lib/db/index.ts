91bd3e28abc03636b02edd6e4a884fba8a874e30
1010mark <markmentaro@yahoo.co.jp>
2025-03-22 16:10:26 +0900

    ssl

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -8,10 +8,10 @@ export const pool = new Pool({
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  ssl:
-    process.env.NODE_ENV === 'production'
-      ? { rejectUnauthorized: false }
-      : false,
+  ssl: true,
+  extra: {
+    ssl: { rejectUnauthorized: false } 
+  }
 });
 
 // 接続テスト
